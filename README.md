dbe78e18deafd1e53ff98385ac87710a4e3e1bce
Nazneen Samu Maricar <nazneenmaricar@Nazs-MacBook-Pro.local>
2017-04-28 14:01:46 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# ra-project02
-New York Times
+# ra-project02 (New York Times)
+
+--Monday (23rd-27th April 2017)
+  Created the Ajax call.
+    -Created the function to contain the call.
+    -Defined the URL to make the call.
+    -Make the call.
+    -Console logged the response.
+
+--Parsed Jason from API.
+    -Dint have to do this step as it was Parsed and created object.
+
+-- Loop and select
+    -Created loop to pull the required data from the object. For this project its the abstract, image and title.
+
+    -Created loop which generates 12 news feeds for any category selected.
+
+--Output info to HTML
+    -Injecting HTML from a loop.
+    -Cloned HTML and JQuery to view every article.
+
+--Created secondary API call for user interaction
+    -Making the new feed generate according to the category a user selects
+
+-- Used SASS and CSS to style the webpage.
+
+--Minifies and cleared the Css sheet.
--- mainjava.js
+++ mainjava.js
@@ -6,68 +6,61 @@ $(document).ready(function(){
 
 $("#category").on('change', function() {
 
-
   $(".load").show();
 
 // reszing the height (height auto)
-  document.getElementById("header-height").addEventListener("change", function() {
-  	this.classList.toggle("active");})
+
+  $('#header-height').animate({
+    height: $('#header-height').get(0).scrollHeight
+    }, 1000, function(){
+    $(this).height('auto');
+  });
 
     selectedSection = $(this).val();
     var selecturl = url + selectedSection + ".json" + '?' + $.param({'api-key': "f5f0c6a8c360474eb8bc1d9d48ad5795"});
 
-  $.ajax({
-      url: selecturl,
-      method: 'GET',
-  })
-  .done(function(result) {
-    $('.contentArea').empty();
-    article (result.results);
-    $(".load").hide();
-  })
-  .fail(function(err) {
-    throw err;
+    $.ajax({
+        url: selecturl,
+        method: 'GET',
+    })
+    .done(function(result) {
+      $('.contentArea').empty();
+      article (result.results);
+      $(".load").hide();
+    })
+    .fail(function(err) {
+      throw err;
+    });
   });
 });
-});
+
+      function newsArticle (abstract, image, link) {
+          var clone=$("#clone").clone();
+              $(clone).children(".article").css("background", "url('"+image+"') 50%");
+              $(clone).find("p").text(abstract);
+              $(clone).find("a").attr("href", link);
+              $(".contentArea").append($(clone).html());
+      }
 
 
+      function article(results) {
 
-function newsArticle (abstract, image, link) {
+        var remIndex=0;
+        for (var i=0; i<results.length; i++){
+        var media=results[i].multimedia[4];
+  // the value is where the meida url is assigned in the API
 
-    var clone=$("#clone").clone();
+  // step by step checking is done for the meida field before populating the article so that the console doesnt throw an error when there is less that 12 articles with image.
 
-        $(clone).children(".article").css("background", "url('"+image+"') no-repeat 50%");
-        $(clone).find("p").text(abstract);
-        $(clone).find("a").attr("href", link);
-        $(".contentArea").append($(clone).html());
-}
-
-
-function article(results) {
-
-  var remIndex=0;
-
-  for (var i=0; i<results.length; i++){
-
-    var media=results[i].multimedia[4]; // the value is where the meida url is assigned in the API
-
-// step by step checking is done for the meida field before populating the article so that the console doesnt throw an error when there is less that 12 articles with image.
-
-    if (media!=undefined && media.hasOwnProperty("url") && media.url!=undefined){
-      newsArticle (results[i].abstract, results[i].multimedia[4].url, results[i].url);
-      remIndex++;}
-      console.log(remIndex);
-      if (remIndex==12){
-      break;
+      if (media!=undefined && media.hasOwnProperty("url") && media.url!=undefined){
+        newsArticle (results[i].abstract, results[i].multimedia[4].url, results[i].url);
+        remIndex++;}
+        console.log(remIndex);
+        if (remIndex==12){
+        break;
     }
   }
 }
-
-
-
-
-
 
 
 
@@ -91,3 +84,20 @@ function article(results) {
 // }).fail(function(err) {
 //   throw err;
 // });
+
+
+
+
+/* there is a bugger, on the 3rd count the height goes back to default*/
+
+   //document.getElementById("header-height").addEventListener("change", function() {
+  // 	this.classList.toggle("active");})
+
+// reszing the height (height auto)
+
+/* animated autoHeight*/
+
+  // var el = $('#header-height'),
+  //     curHeight = el.height(),
+  //     autoHeight = el.css('height', 'auto').height();
+  // el.height(curHeight).animate({height: autoHeight}, 1000);
